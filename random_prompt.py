c68e495c731c4787f517a967a2b5711de527e59e
Abe <>
2023-04-05 22:17:01 +0100

    Switched to non-blocking keyboard input.

--- random_prompt.py
+++ random_prompt.py
@@ -1,60 +1,71 @@
 import sys
 import random
 import time
-import keyboard
+import select
+import platform
 
-# Set the file path of the text file containing the list of prompts
+# ////////////////////////////
+# To Do : Refactor the .txt file-parsing code into a singer parse function.
+# ////////////////////////////
+
 file_path = "prompts.txt"
 
-# Read each line from the text file into an array
 with open(file_path, "r") as f:
     sentences = f.readlines()
 
 # Remove leading and trailing whitespace characters from the string
 sentences = [s.strip() for s in sentences]
 
-# Store an index of chance of appearance in an array mapping to the phrase list
-chance_idx = []
+weight_idx = []
 i = 0
 for s in sentences:
     if ":" in s:
         num_pos = (sentences[i]).index(":") + 1
-        chance_idx.append(int(s[num_pos]))
+        weight_idx.append(int(s[num_pos]))
         s = s[:num_pos-1]
         sentences[i] = s
     else:
-        chance_idx.append(1)
-    # print("sentence: " + sentences[i] + ", chance: " + str(chance_idx[i]) + "\n")
+        weight_idx.append(1)
     i = i + 1
 
 sentences_weighted = []
 for i, s in enumerate(sentences):
-    for chance in range(chance_idx[i]):
+    for chance in range(weight_idx[i]):
         sentences_weighted.append(s)
 
 interval = int(input("Enter the interval time in seconds: "))
+next_run = time.monotonic()
 
+old_settings = termios.tcgetattr(sys.stdin)
+tty.setcbreak(sys.stdin.fileno())
+# Puts the terminal into "cbreak" mode:
+# input characters are delivered to the program as soon as they're entered,
+# rather than being buffered and delivered in batches.
+# This ensures that the program can detect keyboard input as soon as it's entered,
+# rather than waiting for a newline character or other input event.
 
 last_choice = None
 running = True
 while running:
-    if keyboard.is_pressed('q'):
-        running = False
-        print('Exiting program...')
+    if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+        input_char = sys.stdin.read(1)
+        if input_char == 'q':
+            running = False
+            print("Exiting program...")
 
-    this_choice = random.choice(sentences_weighted)
- 
-    # print("this_choice (before): " + this_choice)
-    # if last_choice:
-    #     print("last_choice: " + str(last_choice))
-
-    # Retry randomisation if a consecutive repeat occurs 
-    while (this_choice == last_choice):
-        # print("this_choice == last_choice")
+    if time.monotonic() >= next_run:
         this_choice = random.choice(sentences_weighted)
 
-    last_choice = this_choice
+        # Retry randomisation if a consecutive repeat occurs 
+        while (this_choice == last_choice):
+            this_choice = random.choice(sentences_weighted)
 
-    print(this_choice)
+        last_choice = this_choice
 
-    time.sleep(interval)
+        print(this_choice)
+
+        next_run = time.monotonic() + interval
+
+    time.sleep(0.01)
+
+termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
